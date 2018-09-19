b0b312144ac3e3769ea8154c8e7c0f5180000779
Ekaterina Adamenko <katya.adamenko@gmail.com>
2018-09-19 18:43:26 +0300

    fix

--- cargocult/urls.py
+++ cargocult/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from .views import main_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('^$', main_page),
 ]

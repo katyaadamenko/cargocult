848d9789672dd4e66b3e990a675fcb7abab97a4b
V G <vladislav.goncharenko@phystech.edu>
2018-09-24 23:06:55 +0300

    merge bug fix + little cleanup

--- cargocult/urls.py
+++ cargocult/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 
 from .views import (
+    IndexView,
     TrackViewSet,
     PointViewSet,
     RouteViewSet,
@@ -20,10 +21,10 @@ router.register(r'licences', LicenseViewSet)
 
 
 urlpatterns = [
-    url(r'orders/$', OrdersView.as_view()),
+    url(r'^index/$', IndexView.as_view()),
 
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api/orders/$', OrdersView.as_view()),
+    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     # url(r'^add_file/', add_file),
-    url(r'index/', IndexView.as_view())
 ]
